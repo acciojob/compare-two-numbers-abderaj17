0bd5fba503b2a5161a34e3120fab2b42a363b103
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-06 21:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function compareInput(){
+	const input1 = prompt("Enter first input")
+	const input2 =  promp("Enter second input");
+
+	if(input1 == input2){ 
+		alert("true");
+	}else{
+		alert("false");
+	}
+}
+compareInput();
